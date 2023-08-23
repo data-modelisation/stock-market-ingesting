c0a9dc5cee71426ecec85b2217e2d6b99100bc29
Olga <otolstolutska@gmail.com>
2023-08-23 22:09:53 +0200

    Add big query

--- gcp_project/query.sh
+++ gcp_project/query.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+# Exit immediately if any command fails
+set -o errexit
+
+# Load variables from .env file
 source .env
 
-bq query --nouse_legacy_sql \  
-"SELECT  
-    COUNT(*)  
-FROM  
-   \`${PROJECT_ID}.${DATASET}.*\`;"
+# Construct the SQL query
+query="SELECT * FROM \`${PROJECT_ID}.${DATASET}.market_data_raw*\` LIMIT 10;"
 
+# Print the SQL query (optional)
+echo "Running query : $query"
+
+# Run the BigQuery query
+bq query --nouse_legacy_sql "$query"
+
+# Construct the SQL query
+query="SELECT COUNT(*) as nb_rows FROM \`${PROJECT_ID}.${DATASET}.market_data_raw*\`;"
+
+# Print the SQL query (optional)
+echo "Running query : $query"
+
+# Run the BigQuery query
+bq query --nouse_legacy_sql "$query"

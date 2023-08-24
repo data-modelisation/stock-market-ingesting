dbf7416d3006983efd459f082a26da7bb1ec6996
Olga <otolstolutska@gmail.com>
2023-08-24 18:08:53 +0200

    Change annotation for variable bucket_name

--- gcp_project/setup.sh
+++ gcp_project/setup.sh
@@ -4,21 +4,21 @@
 set -o errexit
 
 # Create bucket
-gsutil ls gs://$BUCKET_NAME || gsutil mb -l $REGION gs://$BUCKET_NAME
-echo "Bucket '$BUCKET_NAME' has been successfully created"
+gsutil ls gs://${BUCKET_NAME} || gsutil mb -l $REGION gs://${BUCKET_NAME}
+echo "Bucket '${BUCKET_NAME}' has been successfully created"
 #  enable uniform bucket-level access on a bucket
 # it enforces a more uniform and consistent access control model
 gsutil uniformbucketlevelaccess set on gs://$BUCKET_NAME
 
 # Create a new service account
-gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
+gcloud iam service-accounts create ${SERVICE_ACCOUNT_NAME} \
     --display-name="market data ingest" \
     --description="description of the market data ingest"
 
 # Add rights for account :
 # - make the service account the admin of the bucket
 # - it can read/write/list/delete etc. on only this bucket
-gsutil iam ch serviceAccount:${SERVICE_ACCOUNT_EMAIL}:roles/storage.admin gs://$BUCKET_NAME
+gsutil iam ch serviceAccount:${SERVICE_ACCOUNT_EMAIL}:roles/storage.admin gs://${BUCKET_NAME}
 
 # - ability to create/delete partitions etc in BigQuery table
 # bq --project_id=${PROJECT_ID} query --nouse_legacy_sql \
